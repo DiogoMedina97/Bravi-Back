81d2e85094a3e6d52104c449c8da55cae28c4126
DiogoMedina97 <diogo_medina97@hotmail.com>
2022-10-26 02:20:43 -0300

    feat(person): add relationship

--- src/person/person.entity.ts
+++ src/person/person.entity.ts
@@ -1,7 +1,15 @@
-import { Column, Entity, PrimaryGeneratedColumn, Repository } from 'typeorm';
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  Repository,
+  OneToMany,
+} from 'typeorm';
 
 import { CreateDTO, UpdateDTO } from './person.dto';
 import { PersonEntityInterface } from './person.interface';
+
+import { ContactEntity } from '../contact/contact.entity';
 
 @Entity('person')
 export class PersonEntity implements PersonEntityInterface {
@@ -13,6 +21,11 @@ export class PersonEntity implements PersonEntityInterface {
 
   @Column({ type: 'varchar', nullable: false })
   surname: string;
+
+  @OneToMany(() => ContactEntity, (contact) => contact.person, {
+    cascade: ['insert', 'update', 'remove'],
+  })
+  contacts: ContactEntity[];
 
   constructor(dto: CreateDTO) {
     if (!dto) return;
