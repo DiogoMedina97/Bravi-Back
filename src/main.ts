2d00863f7556dfe985b282c8ee62ae51ece4cce5
DiogoMedina97 <diogo_medina97@hotmail.com>
2022-10-26 23:43:24 -0300

    feat(main): add dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { config } from 'dotenv';
 import { AppModule } from './app.module';
+
+const env = config().parsed || {};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(env.PORT);
 }
 bootstrap();
